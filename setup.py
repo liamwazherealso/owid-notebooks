36370512449554e9f6da10e291601364211712ea
Liam Pieri <liamwazherealso@gmail.com>
2020-11-27 10:45:02 -0500

    Init commit - cookiecutter struct

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Jupyter for OWID data sets',
+    author='Liam Pieri',
+    license='',
+)
